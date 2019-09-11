3a9d70ecb6dc792ed0287b05e1babc2e432f62e5
Emily Haynes <ehayne00@hotmail.com>
2019-09-11 18:39:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    return hash[key]+=1
+  if hash[:count]
+    return hash[:count]+=1
   else
-   hash[key] = 1
+   hash[:count] = 1
   end
 end
